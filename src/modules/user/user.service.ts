acd9b1b28284acf844ab7ad593d069f9d23408dd
Shubham Singh <shubham.singh@tekinspirations.com>
2025-11-06 21:45:20 +0530

    pagination

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,12 +4,14 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { ILike, In, Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from './entities/user.entity';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { Role } from '../role/entitity/role.entity';
+import { first } from 'rxjs';
+import { lstatSync } from 'fs';
 
 @Injectable()
 export class UserService {
@@ -41,10 +43,51 @@ export class UserService {
   }
 
 
-  async getAllUsers(): Promise<User[]> {
+ /*  async getAllUsers(skip : number, limit : number, search?: string): Promise<User[]> {
+    const where = search ? [
+      { firstName: ILike(`%${search}%`) },
+      { lastName: ILike(`%${search}%`) },
+    ] : {};
+
     return this.userRepository.find({
+      where,
+      skip,
+      take: limit,
       order: { createdAt: 'DESC' },
     });
+  } */
+
+ async getAllUsers( skip: number, limit: number, search?: string, joinRoles = false): Promise<User[]> {
+    const query = this.userRepository
+      .createQueryBuilder('user')
+      .orderBy('user.createdAt', 'DESC')
+      .skip(skip)
+      .take(limit);
+    if (search) {
+      query.andWhere(
+        `(user.firstName LIKE :search OR user.lastName LIKE :search OR CONCAT(user.firstName, ' ', user.lastName) LIKE :search)`,
+        { search: `%${search}%` }
+      );
+    }
+
+    if (joinRoles) {
+      query.leftJoinAndSelect('user.roles', 'roles');
+    }
+
+    return await query.getMany();
+  }
+
+  async getTotalUsersCount(search?: string): Promise<number> {
+    const query = this.userRepository
+      .createQueryBuilder('user');
+
+    if (search) {
+      query.andWhere(
+        `(user.firstName LIKE :search OR user.lastName LIKE :search OR CONCAT(user.firstName, ' ', user.lastName) LIKE :search)`,
+        { search: `%${search}%` }
+      );
+    }
+    return await query.getCount();
   }
 
   async findByEmail(email: string): Promise<User | null> {
@@ -63,12 +106,8 @@ async updateUser(id: string, input: UpdateUserInput): Promise<User> {
   Object.assign(user, input, { updatedAt: new Date() });
 
   if (input.roleIds?.length) {
-  console.log('roleIds coming in:', input.roleIds);
-const roles = await this.roleRepository.find({ where: { id: In(input.roleIds) } });
-console.log('roles found:', roles);
-console.log('user before assign:', user);
-user.roles = roles;
-console.log('user.roles set to:', user.roles);
+    const roles = await this.roleRepository.find({ where: { id: In(input.roleIds) } });
+    user.roles = roles;
   } else {
     user.roles = [];
   }
