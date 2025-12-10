91985c88d045663103ac7c1ade13cca7339e60e6
Shubham Singh <shubham.singh@tekinspirations.com>
2025-12-10 19:38:42 +0530

    aaded dropdown

--- Controllers/DropdownController.cs
+++ Controllers/DropdownController.cs
@@ -1,5 +1,6 @@
 using Infusive_back.EntityData;
 using Microsoft.AspNetCore.Mvc;
+using static Infusive_back.Controllers.IndustryTypeController;
 
 namespace Infusive_back.Controllers
 {
@@ -12,7 +13,6 @@ namespace Infusive_back.Controllers
 
         [HttpGet]
         [Route("getRoles")]
-
         public IActionResult GetRole()
         {
             try
@@ -29,6 +29,136 @@ namespace Infusive_back.Controllers
             catch (Exception ex)
             {
                 return StatusCode(500, new { error = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("IndustryType")]
+        public IActionResult GetIndustryTypes()
+        {
+            try
+            {
+
+                var query = (from ityp in db.IndustryType
+                             select new
+                             {
+                                 IndustryId = ityp.Id,
+                                 IndustryName = ityp.IndustryName ?? "",
+                             });
+
+                return Ok(query);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("country")]
+        public IActionResult GetCountryDropdown()
+        {
+            try
+            {
+                var countries = db.Country.Select(r => new {
+                    CountryId = r.Id,
+                    CountryName = r.Name ,
+                }).ToList();
+                return Ok(countries);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    success = false,
+                    error = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("state/{countryId}")]
+        public IActionResult GetStatesByCountry(int countryId)
+        {
+            try
+            {
+                if (countryId <= 0)
+                {
+                    return BadRequest(new { error = "Country ID is required" });
+                }
+
+                var states = db.State
+                    .Where(s => s.CountryId == countryId)
+                    .Select(s => new
+                    {
+                        StateId = s.Id,
+                        StateName = s.Name
+                    }).ToList();
+
+                return Ok(states);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    success = false,
+                    error = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("city/{stateId}")]
+        public IActionResult GetCitiesByState(int stateId)
+        {
+            try
+            {
+                if (stateId <= 0)
+                {
+                    return BadRequest(new { error = "State ID is required" });
+                }
+
+                var citites = db.City
+                    .Where(s => s.StateId == stateId)
+                    .Select(s => new
+                    {
+                        CityId = s.Id,
+                        CityName = s.Name
+                    }).ToList();
+
+                return Ok(citites);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    success = false,
+                    error = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("countrycodedropdown")]
+        public IActionResult GetCountryCodeDropdown()
+        {
+            try
+            {
+                var countries = db.Country.Select(r => new
+                {
+                    CountryId = r.Id,
+                    countryName = r.Name,
+                    flag = r.FlagUrl,
+                    dialCode = r.DialCode,
+                    code = r.Code,
+
+                }).ToList();
+                return Ok(countries);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    success = false,
+                    error = ex.Message
+                });
             }
         }
     }
