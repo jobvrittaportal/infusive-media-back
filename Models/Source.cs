7e138293b04a93809a9400cb46307a96d3afadee
Shivam12Aryan <aryan.singh@tekinspirations.com>
2025-12-12 17:22:28 +0530

    create source and status model

---
+++ Models/Source.cs
@@ -0,0 +1,9 @@
+﻿namespace Infusive_back.Models
+{
+    public class Source : Base
+    {
+        public required string SourceName { get; set; }
+        public bool? Status { get; set; } = true;
+
+    }
+}
---
+++ Models/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace Infusive_back.Models
+{
+    public class Status : Base
+    {
+        public required string StatusName { get; set; }
+        public bool? IsActive { get; set; } = true;
+
+    }
+}
